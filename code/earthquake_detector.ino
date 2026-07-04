f9128770cdd20345446ff34aef24d1db0a1222f7
Tajkia Zareen <64721699+Zareen548@users.noreply.github.com>
2026-07-04 15:33:06 +0200

    earthquake_detector.ino

--- code/earthquake_detector.ino
+++ code/earthquake_detector.ino
@@ -1,1 +1,38 @@
+const int sensorPin = A0;
+const int buzzerPin = 10;
 
+int threshold = 500;
+unsigned long vibrationStartTime = 0;
+unsigned long requiredDuration = 1000;
+
+bool vibrationDetected = false;
+
+void setup() {
+  pinMode(sensorPin, INPUT);
+  pinMode(buzzerPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int sensorValue = analogRead(sensorPin);
+  Serial.println(sensorValue);
+
+  if (sensorValue > threshold) {
+    if (!vibrationDetected) {
+      vibrationDetected = true;
+      vibrationStartTime = millis();
+    }
+
+    if (millis() - vibrationStartTime >= requiredDuration) {
+      digitalWrite(buzzerPin, HIGH);
+      delay(2000);
+      digitalWrite(buzzerPin, LOW);
+
+      vibrationDetected = false;
+    }
+  } 
+  else {
+    digitalWrite(buzzerPin, LOW);
+    vibrationDetected = false;
+  }
+}
